e6ae30c0615eea19378ac0b10ec5ff648d409955
Daniel de la Torre <galo.delatorre@thoughtworks.com>
2023-03-10 12:44:53 -0500

    feat: specify different instance_type for dev

--- demo.tf
+++ demo.tf
@@ -1,7 +1,7 @@
 #DAN- EC2 instance
 resource "aws_instance" "myEC2" {
   ami           = "ami-0aa7d40eeae50c9a9"
-  instance_type = "t2.micro"
+  instance_type = "t2.nano"
 
   tags = {
     Name = "DanInstance2023-module"
